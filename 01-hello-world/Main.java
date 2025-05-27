e2495322e840d1a0a4cb468e845aa54b6e1a5577
MaNaSDeo <rmnslogin@gmail.com>
2025-05-27 08:51:57 +0530

    hello world

---
+++ 01-hello-world/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+    // public - visible to everyone, 
+    // class - Defines a blueprint,
+    // Main - name of the class, file name must match this when public
+    public static void main(String[] args) {
+        // static - doesn't need to create an object to call this method,
+        // void - this function will not return anything.
+        // main - specail method: it's the entry point for every java application
+        // String[] args - allows command line argument
+        System.out.println("Hello World");
+        // System - built-in class that provides access to system-level stuff.
+        // out - standard output stream
+        // println - prints with new line
+    }
+}
